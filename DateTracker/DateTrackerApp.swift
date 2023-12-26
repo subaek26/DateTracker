0d7f86e0f5edb21c821acd173c35edc3bd75230f
subaek26 <subaek@davidson.edu>
2023-12-25 19:10:47 -0500

    Initial Commit

---
+++ DateTracker/DateTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateTrackerApp.swift
+//  DateTracker
+//
+//  Created by Sunny Baek on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct DateTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
